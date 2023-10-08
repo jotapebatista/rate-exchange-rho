118f44b4e765ab83af98c89152e51fd91efe6a38
João Batista <joao-oliveirabatista@hotmail.com>
2023-10-08 18:52:29 +0100

    init project structure with start.spring.io

---
+++ src/main/java/com/rho/rateexhchange/RateExchangeRhoApplication.java
@@ -0,0 +1,14 @@
+package com.rho.rateexhchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+public class RateExchangeRhoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateExchangeRhoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rho/rateexhchange/RateExchangeRhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rho.rateexhchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateExchangeRhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
